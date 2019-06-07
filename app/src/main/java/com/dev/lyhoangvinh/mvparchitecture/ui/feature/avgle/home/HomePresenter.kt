ea115fb45b26ccc310653d3430325274e60e0e43
Ly Hoang Vinh <vinh270795@gmail.com>
2019-06-07 20:42:10 +0700

    no message

--- app/src/main/java/com/dev/lyhoangvinh/mvparchitecture/ui/feature/avgle/home/HomePresenter.kt
+++ app/src/main/java/com/dev/lyhoangvinh/mvparchitecture/ui/feature/avgle/home/HomePresenter.kt
@@ -6,14 +6,22 @@ import com.dev.lyhoangvinh.mvparchitecture.data.repo.HomeRepo
 import com.dev.lyhoangvinh.mvparchitecture.di.qualifier.ActivityContext
 import com.dev.lyhoangvinh.mvparchitecture.di.scopes.PerFragment
 import com.dev.lyhoangvinh.mvparchitecture.ui.base.presenter.BasePresenter
+import com.dev.lyhoangvinh.mvparchitecture.utils.ConnectionLiveData
 import javax.inject.Inject
 
 @PerFragment
-class HomePresenter @Inject constructor(@ActivityContext ctx: Context, private val homeRepo: HomeRepo) :
+class HomePresenter @Inject constructor(
+    @ActivityContext ctx: Context, private val homeRepo: HomeRepo,
+    private val connectionLiveData: ConnectionLiveData
+) :
     BasePresenter<HomeView>(ctx) {
 
     fun getData() {
-        execute(homeRepo.getRepoHome())
+        connectionLiveData.observe(getLifeCircleOwner(), Observer {
+            if (it!!.isConnected) {
+                execute(homeRepo.getRepoHome())
+            }
+        })
         homeRepo.liveCategories().observe(getLifeCircleOwner(), Observer {
             getView()?.swapCategoriesSuccess(it!!)
         })
